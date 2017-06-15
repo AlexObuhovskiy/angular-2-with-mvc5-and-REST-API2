54080d6eadcd56683ec7c59d8e2c6bb007c987c9
Oleksandr Obukhivskyi <alexobuhovskiy@gmail.com>
2017-06-15 22:47:14 +0300

    picture source added

--- Angular+MVC/app/common/grid.component.ts
+++ Angular+MVC/app/common/grid.component.ts
@@ -1,37 +1,28 @@
 ﻿import { Component, OnInit, Input } from "@angular/core";
 import Product = require("../nodes/home/models/product");
 import ProductType = Product.ProductType;
-import WoodType = Product.WoodType;
 
 @Component({
-	selector: 'grid',
+    selector: 'grid',
     template: `
 		<div *ngFor="let product of products">
             <p>{{product.Id}}</p>
-			<p>{{getProductTypeString(product.Type)}}</p>
-			
+			<p>{{product.TypeAsString}}</p>
+			<img [src]="getImgSource(product)"/>
+            
         </div>
 	`
 })
 export class GridComponent {
     @Input() products: Product.ProductModel;
 
-    private getProductTypeString(productType: Product.ProductType): string {
-        switch (productType) {
+    getImgSource(product: Product.ProductModel) {
+        switch (product.Type) {
             case ProductType.Platbands:
-                return 'Налічник';
-            case ProductType.Bead:
-                return 'Штапік';
-            case ProductType.Lining:
-                return 'Вагонка';
-            case ProductType.Plinth:
-                return 'Плінтус';
+                return '../../images/users.png';
             default:
-                return 'Невідомий';
+                return '';
         }
     }
 
-	private getWoodTypeSring(woodType: WoodType): string {
-	    return '';
-	}
 }
